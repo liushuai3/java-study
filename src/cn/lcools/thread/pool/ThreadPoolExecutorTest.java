c7d0ed2a6e3700acb8ecdcb59c93b8019959ce74
liushuai3 <liushuai3@asiainfo.com>
2019-07-23 16:41:06 +0800

    Thread ThreadPoolExecutorTest

---
+++ src/cn/lcools/thread/pool/ThreadPoolExecutorTest.java
@@ -0,0 +1,27 @@
+package cn.lcools.thread.pool;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by liushuai3 on 2019/7/23.
+ */
+public class ThreadPoolExecutorTest {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                5, // 核心池的大小（即线程池中的线程数目大于这个参数时，提交的任务会被放进任务缓存队列）
+                10, // 线程池最大能容忍的线程数
+                200, //线程存活时间
+                TimeUnit.MILLISECONDS, //参数keepAliveTime的时间单位
+                new ArrayBlockingQueue<Runnable>(5) //任务缓存队列，用来存放等待执行的任务
+        );
+        for (int i = 0; i < 15; i++) {
+            MyTask myTask = new MyTask(i);
+            threadPoolExecutor.execute(myTask);
+            System.out.println("线程池中线程数目："+threadPoolExecutor.getPoolSize()+"，队列中等待执行的任务数目："+
+                    threadPoolExecutor.getQueue().size()+"，已执行完的任务数目："+threadPoolExecutor.getCompletedTaskCount());
+        }
+        threadPoolExecutor.shutdown();
+    }
+}
