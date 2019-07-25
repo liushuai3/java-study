77d14a2d8f383c65884889f6129f7e9e09d42db3
liushuai3 <liushuai3@asiainfo.com>
2019-07-25 14:57:52 +0800

    Thread Classloader

--- src/cn/lcools/jvm/MyClassLoaderTest.java
+++ src/cn/lcools/jvm/MyClassLoaderTest.java
@@ -11,11 +11,30 @@ public class MyClassLoaderTest {
         System.out.println("父加载器："+myClassLoader.getParent());
         Class class1 = myClassLoader.loadClass("cn.lcools.nio.client.AClient");
         System.out.println(class1);
+        System.out.println(class1.hashCode());
         System.out.println("加载器："+class1.getClassLoader());
+        System.out.println("---------------------------");
         MyClassLoader myClassLoader1 = new MyClassLoader("E:\\classloader",myClassLoader);
         System.out.println("父加载器："+myClassLoader1.getParent());
         Class class2 = myClassLoader1.loadClass("cn.lcools.nio.client.AClient");
         System.out.println(class2);
+        System.out.println(class2.hashCode());
         System.out.println("加载器："+class2.getClassLoader());
+        System.out.println("---------------------------");
+        MyClassLoader myClassLoader2 = new MyClassLoader("E:\\classloader",null);
+        System.out.println("父加载器："+myClassLoader2.getParent());
+        Class class3 = myClassLoader2.loadClass("cn.lcools.nio.client.AClient");
+        System.out.println(class3);
+        System.out.println(class3.hashCode());
+        System.out.println("加载器："+class3.getClassLoader());
+
+        System.out.println("---------------------------");
+        MyClassLoader myClassLoader3 = new MyClassLoader("E:\\classloader",myClassLoader2);
+        System.out.println("父加载器："+myClassLoader3.getParent());
+        Class class4 = myClassLoader3.loadClass("cn.lcools.nio.client.AClient");
+        System.out.println(class4);
+        System.out.println(class4.hashCode());
+        System.out.println("加载器："+class4.getClassLoader());
+
     }
 }
